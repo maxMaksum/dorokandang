ce3594ce634d05bce5db02ed1cd8b092cf1bab6f
MenakJinggo <sandangmaksum@gmail.com>
2022-09-15 23:21:41 -0700

    ok new 25

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 
-import {useSession, signIn, signOut, getSession, SessionProvider } from "next-auth/react"
+import {SessionProvider } from "next-auth/react"
 import '../styles/globals.css';
 import { StoreProvider } from "../components/contex/myContext"
 
